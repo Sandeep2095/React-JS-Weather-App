5a8d615e4da90ea1275616c07a749c8a386bcfeb
SANDEEP <sks34246@gmail.com>
2022-01-18 23:34:15 +0530

    Updates

---
+++ precache-manifest.95c07ffdc08b97f32716aa57bda3de33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d9eae335e969441b0fc18e98c54cfe",
+    "url": "/React-JS-Weather-App/index.html"
+  },
+  {
+    "revision": "addfc9c3c453ddd6b16a",
+    "url": "/React-JS-Weather-App/static/css/main.761c37ba.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/React-JS-Weather-App/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "f5527cba62243bf2d608",
+    "url": "/React-JS-Weather-App/static/js/3.358c86c0.chunk.js"
+  },
+  {
+    "revision": "addfc9c3c453ddd6b16a",
+    "url": "/React-JS-Weather-App/static/js/main.58774b60.chunk.js"
+  },
+  {
+    "revision": "b2c30378fb3edd025df4",
+    "url": "/React-JS-Weather-App/static/js/runtime~main.d8f0e1ac.js"
+  },
+  {
+    "revision": "4ec2be15ee7d929bba7997be502e29b3",
+    "url": "/React-JS-Weather-App/static/media/cold-bg.4ec2be15.jpg"
+  },
+  {
+    "revision": "106a3dca28f798f916bc0050dd0cfa7a",
+    "url": "/React-JS-Weather-App/static/media/warm-bg.106a3dca.jpg"
+  }
+]);
